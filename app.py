06db71b563c644052dcf5925064bc92cd9a6269f
bmcilhenny <bmcilhen@gmail.com>
2018-04-04 14:34:24 -0400

    sessions, login and logout added. Navbar changes based on if you're logged in or not

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 # For debugging purposes use ->
 # import pdb; pdb.set_trace()
 
@@ -76,6 +77,107 @@ def register():
         redirect(url_for('index'))
     return render_template('register.html', form=form)
 
+@app.route('/login', methods=['GET', 'POST'])
+
+def login():
+    if request.method == 'POST':
+        # Get form fields, we're not using wtfforms here, hence no form.name.data
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create a cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            #fetch one only grabs the first one in the SQL query
+            data = cur.fetchone()
+            password = data['password']
+
+            if sha256_crypt.verify(password_candidate, password):
+                #Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+
+                app.logger.info('PASSWORD MATCHED')
+
+            else:
+                error = 'Incorrect password'
+                return render_template('login.html', error=error)
+                app.logger.info['INCORRECT PASSWORD']
+
+            #Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+
+
+    return render_template('login.html')
+
+#Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+# Logout
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+# Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
+
+# Article Form Class
+
+class ArticleForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=200)])
+    body = StringField('Body', [validators.Length(min=20)])
+
+# Add Article route
+@app.route('/add_article', methods=['GET', 'POST'])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        #Execute
+        cur.execute("INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)", [title, body, session['username']])
+
+        # Commit to # DEBUG:
+        mysql.connection.commit()
+
+        #Close connection
+        cur.close()
+
+        flash('Article Created', 'success')
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_article.html', form=form)
 
 if __name__ == '__main__':
     app.secret_key='secret123'
